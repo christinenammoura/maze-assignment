b1de9c566c710240271e72df42256ec93cdb3098
Christine Nammoura <christinejnammoura@gmail.com>
2024-10-26 14:35:37 +0300

    Second commit: declare variables

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+
+var start = document.getElementById('start');
+var end = document.getElementById('end');
+var boundaries = document.querySelectorAll('.boundary');
+var status = document.getElementById('status');
+
+var startGame = false;
+
+function gameStarted(){
+    startGame = true;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
